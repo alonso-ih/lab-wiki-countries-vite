f56da15ea580b06d64fc6d5b53086427ae10cc94
Pablo Alonso <alonso@ironhack.com>
2026-06-23 12:08:31 -0600

    Complete first WikiCountries iteration

--- src/pages/CountryDetailsPage.jsx
+++ src/pages/CountryDetailsPage.jsx
@@ -1,3 +1,9 @@
-function CountryDetails() {}
+function CountryDetailsPage() {
+  return (
+    <div className="container">
+      <p style={{ fontSize: "24px", fontWeight: "bold" }}>Country Details</p>
+    </div>
+  );
+}
 
-export default CountryDetails;
+export default CountryDetailsPage;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,11 @@
-function HomePage() {}
+function HomePage() {
+  return (
+    <div className="container" style={{ maxHeight: "90vh", overflow: "scroll" }}>
+      <h1 style={{ fontSize: "24px" }}>
+        WikiCountries: Your Guide to the World
+      </h1>
+    </div>
+  );
+}
 
 export default HomePage;
