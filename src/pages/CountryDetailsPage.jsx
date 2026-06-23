339665e80cf513d87c0ca2bb9c48b720b291a915
Pablo Alonso <alonso@ironhack.com>
2026-06-23 12:12:40 -0600

    Complete WikiCountries iterations

--- src/pages/CountryDetailsPage.jsx
+++ src/pages/CountryDetailsPage.jsx
@@ -1,7 +1,65 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+
 function CountryDetailsPage() {
+  const [country, setCountry] = useState(null);
+  const { countryId } = useParams();
+
+  useEffect(() => {
+    setCountry(null);
+
+    axios
+      .get(`https://ih-countries-api.herokuapp.com/countries/${countryId}`)
+      .then((response) => {
+        setCountry(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [countryId]);
+
+  if (!country) {
+    return (
+      <div className="container">
+        <p style={{ fontSize: "24px", fontWeight: "bold" }}>Country Details</p>
+        <p>Loading</p>
+      </div>
+    );
+  }
+
   return (
     <div className="container">
       <p style={{ fontSize: "24px", fontWeight: "bold" }}>Country Details</p>
+      <h1>{country.name.common}</h1>
+
+      <table className="table">
+        <tbody>
+          <tr>
+            <td style={{ width: "30%" }}>Capital</td>
+            <td>{country.capital && country.capital[0]}</td>
+          </tr>
+          <tr>
+            <td>Area</td>
+            <td>
+              {country.area} km<sup>2</sup>
+            </td>
+          </tr>
+          <tr>
+            <td>Borders</td>
+            <td>
+              <ul>
+                {country.borders &&
+                  country.borders.map((border) => (
+                    <li key={border}>
+                      <Link to={`/${border}`}>{border}</Link>
+                    </li>
+                  ))}
+              </ul>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,48 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
 function HomePage() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://ih-countries-api.herokuapp.com/countries")
+      .then((response) => {
+        setCountries(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <div className="container" style={{ maxHeight: "90vh", overflow: "scroll" }}>
       <h1 style={{ fontSize: "24px" }}>
         WikiCountries: Your Guide to the World
       </h1>
+
+      <div className="list-group">
+        {countries.map((country) => {
+          const flagUrl = `https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`;
+
+          return (
+            <Link
+              className="list-group-item list-group-item-action"
+              key={country.alpha3Code}
+              to={`/${country.alpha3Code}`}
+            >
+              <img
+                alt={`${country.name.common} flag`}
+                className="me-2"
+                src={flagUrl}
+                style={{ width: "24px" }}
+              />
+              {country.name.common}
+            </Link>
+          );
+        })}
+      </div>
     </div>
   );
 }
